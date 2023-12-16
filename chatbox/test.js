8e93f6aad45ca98b596899b6835eabb8c2a69cc9
CinnamonTeaa <shervinelibox569@gmail.com>
2023-12-15 21:30:11 -0400

    updates

--- chatbox/test.js
+++ chatbox/test.js
@@ -1,27 +1,5 @@
+import { ChatCompletion } from 'openai-api';
 
-const express = require('express');
-const openai = require('openai'); 
-
-const app = express();
-
-
-const openaiAPI = new openai.OpenAI('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'); 
-
-
-app.post('/ask-gpt', async (req, res) => {
-    const userInput = req.body.message;
-
-  
-    const gptResponse = await openaiAPI.complete({
-        engine: 'text-davinci-003', 
-        prompt: userInput,
-        max_tokens: 150 
-    });
-
-    
-    res.json({ reply: gptResponse.choices[0].text.trim() });
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const openai = new ChatCompletion({
+ apiKey: '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',
 });
